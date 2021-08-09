0af418ab04624de9612d7d47c072e711e9749714
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-08-09 12:33:24 +0800

    feat: migrate PNG module with alpha channel.

--- src/png/mod.rs
+++ src/png/mod.rs
@@ -1,16 +1,17 @@
 mod png;
 
-use png::PNG;
 use crate::lib::Image;
+use png::PNG;
 
 use std::error::Error;
 
-pub fn save_png<const WIDTH: usize, const HEIGHT: usize>(
+pub fn save_png<const WIDTH: usize, const HEIGHT: usize, const CHANNEL: usize>(
     path: &str,
-    data: [[[u8; 3]; WIDTH]; HEIGHT],
+    data: [[[u8; CHANNEL]; WIDTH]; HEIGHT],
 ) -> Result<(), Box<dyn Error>> {
     let data = data.iter().cloned().flatten().flatten().collect::<Vec<_>>();
-    PNG::new(WIDTH as u32, HEIGHT as u32, false, &data).dump(path)
+    let has_alpha = if CHANNEL == 3 { false } else { true };
+    PNG::new(WIDTH as u32, HEIGHT as u32, has_alpha, &data).dump(path)
 }
 
 #[cfg(test)]
@@ -18,7 +19,7 @@ mod tests {
     use super::save_png;
 
     #[test]
-    fn test_save_png_with_alpha() {
+    fn test_save_png_without_alpha() {
         // Prepare data
         let mut png_array = [[[0_u8; 3]; 255]; 255];
         let mut outer_index: usize = 0;
@@ -35,5 +36,27 @@ mod tests {
             outer_index += 1;
         }
         save_png("./image/rgb.png", png_array).unwrap();
+    }
+
+    #[test]
+    fn test_save_png_with_alpha() {
+        // Prepare data
+        let mut png_array = [[[0_u8; 4]; 255]; 255];
+        let mut outer_index: usize = 0;
+        while outer_index < 255 {
+            let mut inner_index: usize = 0;
+            while inner_index < 255 {
+                png_array[outer_index][inner_index][0] = outer_index as u8;
+                png_array[outer_index][inner_index][1] = inner_index as u8;
+                png_array[outer_index][inner_index][2] = 128;
+                png_array[outer_index][inner_index][3] =
+                    ((outer_index as u16 + inner_index as u16) / 2) as u8;
+
+                inner_index += 1;
+            }
+
+            outer_index += 1;
+        }
+        save_png("./image/rgba.png", png_array).unwrap();
     }
 }
