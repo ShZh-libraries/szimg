8ee618e8cb1a22188469ba89e986649339a23cb7
Sh-Zh-7 <zhihaoshen7@qq.com>
2021-08-15 21:30:26 +0800

    fix: remove save image unit test in top-level module.

--- src/img/png/mod.rs
+++ src/img/png/mod.rs
@@ -23,38 +23,3 @@ pub trait ChecksumIterator {
 
     fn get(&self) -> u32;
 }
-
-#[cfg(test)]
-mod tests {
-    use super::save_png;
-
-    #[test]
-    fn test_save_png_without_alpha() {
-        // Prepare data
-        let mut png_array = [[[0_u8; 3]; 255]; 255];
-        for outer_index in 0..255 {
-            for inner_index in 0..255 {
-                png_array[outer_index][inner_index][0] = outer_index as u8;
-                png_array[outer_index][inner_index][1] = inner_index as u8;
-                png_array[outer_index][inner_index][2] = 128;
-            }
-        }
-        save_png("./image/rgb.png", png_array).unwrap();
-    }
-
-    #[test]
-    fn test_save_png_with_alpha() {
-        // Prepare data
-        let mut png_array = [[[0_u8; 4]; 255]; 255];
-        for outer_index in 0..255 {
-            for inner_index in 0..255 {
-                png_array[outer_index][inner_index][0] = outer_index as u8;
-                png_array[outer_index][inner_index][1] = inner_index as u8;
-                png_array[outer_index][inner_index][2] = 128;
-                png_array[outer_index][inner_index][3] =
-                    ((outer_index as u16 + inner_index as u16) / 2) as u8;
-            }
-        }
-        save_png("./image/rgba.png", png_array).unwrap();
-    }
-}
